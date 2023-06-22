28e6143107b567bf5d808789cda8dd690873dbea
zakotiukk <zakotiukk@gmail.com>
2023-06-22 20:32:35 +0300

    c

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -13,25 +13,6 @@ import img7 from '../../img/7.jpg';
 import img8 from '../../img/8.jpg';
 import img9 from '../../img/9.jpg';
 
-/*const products = [
-    {id: '1', 
-    title: 'Everything I Know About Love', 
-    price: 480,  
-    author: 'Dolly Alderton',
-    description:'Award-winning journalist Dolly Alderton survived her twenties (just about) and in Everything I Know About Love, she gives an unflinching account of the bad dates and squalid flat-shares, the heartaches and humiliations, and most importantly, the unbreakable female friendships that helped her to hold it all together. Glittering with wit, heart and humour, this is a book to press into the hands of every woman who has ever been there or is about to find themselves taking that first step towards the rest of their lives.',
-    pages:368,
-    rating: 3,
-    },
-    {id: '2', title: "Harry potter and the philosopher's stone", price: 340, author:  'J. K. Rowling'},
-    {id: '3', title: 'Dune', price: 600,  author:  'Frank Herbert'},
-    {id: '4', title: '1984', price: 530,  author:  'George Orwell'},
-    {id: '5', title: 'The Night They Vanished', price: 650,  author: 'Vanessa Savage'},
-    {id: '6', title: 'The hound of the baskervilles', price: 150, author:  ' Arthur Conan Doyle'},
-    {id: '7', title: 'Book of night', price: 490,  author:  ' Holly Black'},
-    {id: '8', title: 'Beartown', price: 570, author:  'Fredrik Backman'},
-    {id: '9', title: 'Solstice of Death', price: 820,  author:  'Laurence Anholt'},
-]*/
-
 const products = [
     {id: '1', 
     title: 'Everything I Know About Love', 
@@ -79,22 +60,31 @@ const ProductList = () => {
     const [addedItems, setAddedItems]=useState([]);
     const {tg} =useTelegram();
 
-    const onSendData = useCallback(() => {
+   /* const onSendData = useCallback(() => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
         
-    }, [addedItems])
+    }, [addedItems])*/
+
+    const onSendData = useCallback(() => {
+        const data = {
+          products: addedItems,
+          totalPrice: getTotalPrice(addedItems),
+        };
+        tg.sendData(JSON.stringify(data));
+      }, [addedItems, tg]);
+    
 
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+        };
+    }, [onSendData,tg]);
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
